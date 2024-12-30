fec3f854391d2459a6c71e88c0fccf79f9f8d242
nazal10line@gmail.com <108220376+Nazal10line@users.noreply.github.com>
2024-12-30 21:27:31 +0300

    FeltchFullFinished

--- Lessons/Lesson05-Http-Assync/05-fetch.js
+++ Lessons/Lesson05-Http-Assync/05-fetch.js
@@ -21,3 +21,75 @@ function getApi() {
     .catch((error) => console.log(error))
 }
 getApi();
+
+class Request {
+    static get(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+            .then((response) => response.json())
+            .then((data) => resolve(data))
+            .catch((error) => reject(error));
+        });
+    }
+    static post(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+            .then((response) => response.json())
+            .then((data) => resolve(data))
+            .catch((error) => reject(error));
+        });
+    }
+    static put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: "PUT",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+            .then((response) => response.json())
+            .then((data) => resolve(data))
+            .catch((error) => reject(error));
+        });
+    }
+    static delete(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: "DELETE"
+            })
+            .then((response) => resolve("Deleted"))
+            .catch((error) => reject(error));
+        });
+    }
+}
+
+Request.get("https://jsonplaceholder.typicode.com/posts")
+.then((data) => console.log(data))
+.catch((error) => console.log(error));
+
+Request.post("https://jsonplaceholder.typicode.com/posts",{
+    userId: 1,
+    title: "Post Title",
+    body: "Post Body"
+})
+.then((data) => console.log(data))
+.catch((error) => console.log(error));
+
+Request.put("https://jsonplaceholder.typicode.com/posts/1",{
+    userId: 1,
+    title: "Post Title fdherty",
+    body: "Post Body dyjdtjty"
+})
+.then((data) => console.log(data))
+.catch((error) => console.log(error));
+
+Request.delete("https://jsonplaceholder.typicode.com/posts/1")
+.then((data) => console.log(data))
+.catch((error) => console.log(error));
