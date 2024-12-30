8cb35f887c932ce2bd940e9d7305320e0b417d55
nazal10line@gmail.com <108220376+Nazal10line@users.noreply.github.com>
2024-12-30 09:06:19 +0300

    finishedStorage

--- Lessons/Lesson04-Javascript/3-storage.js
+++ Lessons/Lesson04-Javascript/3-storage.js
@@ -42,3 +42,73 @@ localStorage.setItem("XogtaShaqaalaha",JSON.stringify(Shaqaale));
 // JSON.parse() metodu ile string'i objeye çevirme, Console de gormek icin.
 let XogtaShaqaalaha = JSON.parse(localStorage.getItem("XogtaShaqaalaha"));
 console.log(XogtaShaqaalaha);
+
+let isticmaale = [
+    {name: "Jaamac", age: 34},
+    {name: "Warfaa", age: 42},
+    {name: "Hinda", age:19},
+]
+
+localStorage.setItem("isticmaleDetails", JSON.stringify(isticmaale));
+
+let isticmaleDetails = JSON.parse(localStorage.getItem("isticmaleDetails"));
+
+console.log(isticmaleDetails);
+isticmaleDetails.map((isticmaale) => {
+    document.write(`<p> ${isticmaale.name} - ${isticmaale.age}</p>`)
+});
+
+// Session Storage
+
+let jaariyad = [
+    {name: "Fartuun", age: 24},
+    {name: "Nuux", age: 55},
+    {name: "Hinda", age: 19},
+]
+sessionStorage.setItem("jaariyad", JSON.stringify(jaariyad));
+
+let jaariyadDetails = JSON.parse(sessionStorage.getItem("jaariyad"));
+
+console.log(jaariyadDetails);
+jaariyadDetails.map((jaariyad) => {
+    document.write(`<p> ${jaariyad.name} - ${jaariyad.age}</p>`)
+});
+
+let daYarta = jaariyadDetails.filter((jaariyad) => jaariyad.age <25);
+
+daYarta.map((jaariyad) => {
+    document.write(`<p> ${jaariyad.name} - ${jaariyad.age}</p>`);
+});
+
+
+const tirooyin = [55,37,13,96,32,78,94,93];
+let langs = ["Python", "Java", "C++", "Javascript"];
+
+const natiijo = tirooyin.reduce((tiroGuud, tiro) => {
+    return tiroGuud + tiro;
+}, 0);
+
+console.log(natiijo);
+
+//Spread Operator iki dizi birlestirmek.
+const natiijo2 = [...langs, ...tirooyin];
+console.log(natiijo2);
+
+langs = [...langs, "C#"];
+console.log(langs);
+
+
+// Destructing Assignment
+let ShaqaaleCopy = {
+    name: "Jaamac",
+    da: 23,
+    city: "Vienna",
+    languages: ["Python", "Java", "JavaScript"],
+};
+
+let {name, da, ...intaHartay} = ShaqaaleCopy;
+console.log(name, da, intaHartay);
+
+let tirooyinCopy = [3,5,6,34,21,678,44,99];
+let [a,b, ...c] = tirooyinCopy;
+console.log(a,b,c);
